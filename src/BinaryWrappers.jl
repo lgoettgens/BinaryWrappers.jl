3261625cb82d8c6d0b7555ccef28d0a44b5e40d2
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2026-01-06 13:21:35 +0100

    Refactoring to prepare for changes

--- src/BinaryWrappers.jl
+++ src/BinaryWrappers.jl
@@ -7,52 +7,56 @@ using JLLWrappers
 
 const wrapper_key = "binarywrappers_v$(VERSION.major).$(VERSION.minor)"
 
-# use @generate_wrappers instead to automatically deduce the calling module
-function generate_wrappers(m::Module, caller::Union{Module, Base.UUID, Nothing})
-    # we generate wrappers per minor julia version
-    # the scratch will belong to the jll which the wrappers are generated for
-    # and the usage is tied to the module calling the `@generate` macro.
-    target = get_scratch!(m, wrapper_key, caller)
-     
-    binpath(name) = joinpath(target, "bin", name)
-    mkpath(binpath(""))
-
-    bindir = joinpath(getproperty(m, :artifact_dir), "bin")
-    sourcebinary = joinpath(bindir,"\$(basename \$0)")
-    libpath = getproperty(m, :LIBPATH)[]
-
-    wrapper = """
+function wrapper_contents(libpath::String, sourcebinary::String)
+    return """
         #!/bin/sh
         # Since we cannot run these binaries through the usual julia commands we need
         # this wrapper that sets up the correct library paths.
         export $(JLLWrappers.LIBPATH_env)="$(libpath)"
         exec $(sourcebinary) "\$@"
         """
+end
 
+function shell_script_wrapper_contents(libpath::String, sourcebinary::String)
     # For shell scripts we use `source` (.) instead of exec to avoid macOS stripping
     # the LIBPATH. This means \$0 will point to our wrapper.
     # For 4ti2 this means that its own wrapper scripts will call our wrapper for the
     # binaries, so strictly speaking adjusting the LIBPATH here is not necessary.
-    scriptwrapper = """
+    return """
         # We cannot run shell scripts directly as macOS will remove
         # DYLD_FALLBACK_LIBRARY_PATH for any subshells.
         # So we source the original script instead.
         export $(JLLWrappers.LIBPATH_env)="$(libpath)"
         . $(sourcebinary) "\$@"
         """
+end
+
+# use @generate_wrappers instead to automatically deduce the calling module
+function generate_wrappers(m::Module, caller::Union{Module, Base.UUID, Nothing})
+    # we generate wrappers per minor julia version
+    # the scratch will belong to the jll which the wrappers are generated for
+    # and the usage is tied to the module calling the `@generate` macro.
+    target = get_scratch!(m, wrapper_key, caller)
+
+    binpath(name) = joinpath(target, "bin", name)
+    mkpath(binpath(""))
+
+    bindir = joinpath(getproperty(m, :artifact_dir), "bin")
+    sourcebinary = joinpath(bindir, "\$(basename \$0)")
+    libpath = getproperty(m, :LIBPATH)[]
 
     # POSIX compatible shells
     shellre = r"^#!/bin/(ba|da|z|k)?sh"
 
     for bin in readdir(bindir)
         if isfile(joinpath(bindir, bin))
-            (tmpfile, tmpio) = mktemp(binpath(""),cleanup=false)
-            # shell scripts use a different wrapper because macOS...
-            m = match(shellre, String(read(joinpath(bindir, bin), 11)))
-            if m != nothing
-                write(tmpio, m.match * "\n" * scriptwrapper)
+            (tmpfile, tmpio) = mktemp(binpath(""); cleanup=false)
+            shebang = readline(joinpath(bindir, bin))
+            if match(shellre, shebang) !== nothing
+                # shell scripts use a different wrapper because macOS...
+                write(tmpio, "#!/bin/sh\n" * shell_script_wrapper_contents(libpath, sourcebinary))
             else
-                write(tmpio, wrapper)
+                write(tmpio, wrapper_contents(libpath, sourcebinary))
             end
             close(tmpio)
             chmod(tmpfile, 0o755)
